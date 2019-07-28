d8034847f29cd5b82fdd555399a9fd8a9851960e
Xploitable <dannychaindog@gmail.com>
2019-07-28 13:21:34 +0400

    Code corrections for test_items.py added: check of the button for adding to cart was implemented

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_cart_button(browser):
     browser.get(link)
-    browser.find_element_by_class_name("btn-add-to-basket")
+    button = browser.find_element_by_class_name("btn-add-to-basket")
     #time.sleep(30) # для визуальной проверки языка страницы и кнопки
-    
+    assert button, "There is nothing here"
